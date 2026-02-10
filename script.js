3f8a2877c4527a1c3d764f25a29cabf0171713d9
Ragavi2909 <rrgavi03@gmail.com>
2026-02-10 07:40:49 +0530

    learnt basic in js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage(){
+    let inputBox = document.getElementById("nameInput");
+    let userText = inputBox.value;
+    let output = document.getElementById("outputText");
+    output.innerText = userText;
+
     alert("Button clicked");
 }
